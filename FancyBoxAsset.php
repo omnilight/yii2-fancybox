44404d9afedba11aeaffc3ab21a80ff1b08d3f56
Pavel <pavel.agalecky@gmail.com>
2015-03-11 12:35:37 +0300

    Added more scripts

--- FancyBoxAsset.php
+++ FancyBoxAsset.php
@@ -13,10 +13,12 @@ class FancyBoxAsset extends AssetBundle
     public $sourcePath = '@bower/fancybox/source';
 
     public $js = [
-        'jquery.fancybox.pack.js'
+        'jquery.fancybox.pack.js',
+        'jquery.fancybox-buttons.js',
     ];
     public $css = [
         'jquery.fancybox.css',
+        'jquery.fancybox-buttons.css',
     ];
 
     public $depends = [
